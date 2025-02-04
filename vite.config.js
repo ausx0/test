fb8477bb6bfde124b69b79b17c16c17a6beb243d
ausx0 <ahmedalnaemy112233@gmail.com>
2025-02-04 15:07:31 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/test/",
   plugins: [react()],
-  base: "test",
+  server: {
+    host: true,
+  },
 });
